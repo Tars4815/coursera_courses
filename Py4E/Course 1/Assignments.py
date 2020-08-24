86e0c33b7f368d769865aff8750c73e18b39e24c
Federica Gaspari <34043707+Tars4815@users.noreply.github.com>
2020-08-24 22:38:22 +0200

    Added assignment 2.3

--- Py4E/Course 1/Assignments.py
+++ Py4E/Course 1/Assignments.py
@@ -5,3 +5,12 @@ print("hello world")
 # Enter Sarah in the pop-up box when you are prompted so your output will match the desired output.
 name = input("Enter your name")
 print("Hello "+ name)
+
+#Week 4 - Assignment 2.3: Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25).
+# You should use input to read a string and float() to convert the string to a number. Do not worry about error checking or bad user data.
+hrs = input("Enter Hours:")
+rate = input("Enter Rate:")
+hr = float(hrs)
+rt = float(rate)
+pay = hr*rt
+print("Pay: " + str(pay))
