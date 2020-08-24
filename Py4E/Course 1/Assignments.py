e88049e292da878a9033535eb0a39d2b3c90f18f
Federica Gaspari <34043707+Tars4815@users.noreply.github.com>
2020-08-24 22:37:11 +0200

    Update Assignments.py

--- Py4E/Course 1/Assignments.py
+++ Py4E/Course 1/Assignments.py
@@ -1,2 +1,7 @@
-# Week 1 - Assignment: Write Hello World
+# Week 3 - Assignment: Write Hello World
 print("hello world")
+
+#Week 4 - Assignment 2.2:  Write a program that uses input to prompt a user for their name and then welcomes them. Note that input will pop up a dialog box.
+# Enter Sarah in the pop-up box when you are prompted so your output will match the desired output.
+name = input("Enter your name")
+print("Hello "+ name)
